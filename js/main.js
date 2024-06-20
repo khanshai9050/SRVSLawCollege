28866d40fdbcfb49fdedea2cc35b97887738b32f
khanshai9050 <167965233+khanshai9050@users.noreply.github.com>
2024-06-20 12:58:47 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -79,3 +79,33 @@
     
 })(jQuery);
 
+/*js for work counter*/
+$(document).ready(function () {
+        $.fn.jQuerySimpleCounter = function (options) {
+            var settings = $.extend({
+                start: 0,
+                end: 100,
+                easing: 'swing',
+                duration: 400,
+                complete: ''
+            }, options);
+
+            var thisElement = $(this);
+
+            $({ count: settings.start }).animate({ count: settings.end }, {
+                duration: settings.duration,
+                easing: settings.easing,
+                step: function () {
+                    var mathCount = Math.ceil(this.count);
+                    thisElement.text(mathCount);
+                },
+                complete: settings.complete
+            });
+        };
+        $('#number1').jQuerySimpleCounter({ end: 11, duration: 5000 });
+        $('#number2').jQuerySimpleCounter({ end: 100, duration: 5000 });
+        $('#number3').jQuerySimpleCounter({ end: 2000, duration: 5000 });
+        $('#number4').jQuerySimpleCounter({ end: 6000, duration: 5000 });
+		
+    });
+/*end of js for work counter*/
